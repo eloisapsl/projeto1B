b2f6a5ec9e7a1f60da701cf8df23093c18dff9b4
eloisapsl <111752882+eloisapsl@users.noreply.github.com>
2023-10-21 22:52:21 -0300

    Add files via upload

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -5,17 +5,20 @@ const AuthContext = createContext({});
 export function AuthProvider({children}){
 
     const [logado, setLogado] = useState(false);
+    const [loading, setLoading] = useState(false);
 
     function signIn(){
         return new Promise(resolve =>{
+            setLoading(true);
             setTimeout(function(){
                 setLogado(true);
+                setLoading(false);
             },2000)
         })
     }
 
     return(
-        <AuthContext.Provider value = {{logado, signIn}}>
+        <AuthContext.Provider value = {{logado, signIn, loading}}>
             {children}
         </AuthContext.Provider>
     );
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,9 +2,22 @@ import { useContext } from "react";
 import AuthRoutes from "./AuthRoutes";
 import AppRoutes from "./AppRoutes";
 import AuthContext from "../contexts/AuthContext";
+import { ActivityIndicator, View } from "react-native";
 
 export default function Routes(){
-    const {logado} = useContext(AuthContext);
+    const {logado, loading} = useContext(AuthContext);
+
+    if(loading){
+        return(
+            <View style={{flex: 1,
+                alignItems:'center',
+                justifyContent:'center',
+                backgroundColor:'black'
+            }}>
+                <ActivityIndicator size = {'large'}/>
+            </View>
+        )
+    }
     return(
         logado == true ? <AppRoutes/> : <AuthRoutes/>
     )
